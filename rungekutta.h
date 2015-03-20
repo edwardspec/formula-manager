40e95409acbeb8c17f6dad4f561b12fdde3294d2
Edward Chernenko <edwardspec@gmail.com>
2015-03-20 23:36:35 +0300

    (docs) Improve documentation in <rungekutta.h>

--- rungekutta.h
+++ rungekutta.h
@@ -1,6 +1,6 @@
 /*
 	Formula manager - the mathematical library.
-	Copyright (C) 2010 Edward Chernenko.
+	Copyright (C) 2010-2015 Edward Chernenko.
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -18,14 +18,19 @@
 
 #include "formula.h"
 
-/*
-	Solve a differential equation
-		Y' = F(X, Y)
-	with Y(X0) = Y0, where X0 and Y0 are given.
+/**
+	@brief Solve a differential equation
+		Y' = F(X, Y) with Y(X0) = Y0, where X0 and Y0 are given.
 
-	NOTE: the formula returned is independent from its first parameter
-	(so the F can be later freed, and Y' will continue to be operational).
-	The formula returned must be formula_free()d.
+	@param F Formula F(X, Y) in the right part of the equation.
+	@param X0 Some value of X.
+	@param Y0 Value of Y(X) in point \b X0.
+	@returns Formula object that contains Y(X) function.
+
+	@note The formula returned is independent from its first parameter
+		(so the F can be later freed, and Y will continue to be
+		operational).
+	@note The formula returned must be formula_free()d.
 */
 formula rungekutta_solve(formula F, double X0, double Y0);
 
